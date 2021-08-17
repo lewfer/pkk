7ee0781f53daef8da4527e1fb51fc94649a3f97c
lewfer <34630995+lewfer@users.noreply.github.com>
2021-08-17 12:22:03 +0100

    Update main.blocks, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
-})
+namespace pkk {
+    /**
+     * Test pkk
+     */
+    //% blockId=pkkpoinkk
+    //% block = "plot at index"
+    //% a.min=0 a.max=4
+    export function poinkk(a: number, b: number) : void {
+        const x = a + b;
+        led.plot(a,b);
+    }
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,2 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+pkk.poinkk(4,4);
